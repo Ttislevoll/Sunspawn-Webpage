83e4ffb8cc025a3c4e58e5b7bbbe84874df95ef1
Ttislevoll <torgeir.tislevoll@gmail.com>
2022-05-02 21:13:01 +0200

    added headers

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+
+const App = () => {
+  return (
+    <div>
+      <Header title="Sunspawn" />
+
+    </div>
+  );
+}
+
+export default App;
