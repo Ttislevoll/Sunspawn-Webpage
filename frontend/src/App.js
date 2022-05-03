5ceafdb645eb04778bf10bdb28bf22a2f285c50e
Ttislevoll <torgeir.tislevoll@gmail.com>
2022-05-03 21:38:29 +0200

    image array fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,21 +6,27 @@ import Search from './components/search';
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
-
   const [word, setWord] = useState('');
+  const [images, setImages] = useState([]);
+
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
+
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((err) => {
         console.log(err);
-      })
-  }
+      });
+    setWord('');
+  };
 
   console.log(UNSPLASH_KEY);
   return (
@@ -29,6 +35,6 @@ const App = () => {
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,28 +2,23 @@ import React from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: 'darkred'
-}
+  backgroundColor: 'darkred',
+};
 
 const Header = ({ title }) => {
-
-
-    return (
-        <Navbar style={navbarStyle} variant="dark">
-            <Container>
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
-                <Nav className="me-auto">
-                    <Nav.Link href="#Home">Home</Nav.Link>
-                    <Nav.Link href="#Concerts">Concerts</Nav.Link>
-                    <Nav.Link href="#Merch">Merch</Nav.Link>
-                    <Nav.Link href="#Contact">Contact</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar style={navbarStyle} variant="dark">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#Home">Home</Nav.Link>
+          <Nav.Link href="#Concerts">Concerts</Nav.Link>
+          <Nav.Link href="#Merch">Merch</Nav.Link>
+          <Nav.Link href="#Contact">Contact</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
-
-
-
